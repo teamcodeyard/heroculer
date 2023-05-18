4cdd5b8a46a05842117cdf6e3694360f5ea856d0
Imre Ujlaki <ujlaki.imre@gmail.com>
2023-05-18 15:08:25 +0200

    :sparkles: Deploy service by name

--- src/commands/deploy/index.ts
+++ src/commands/deploy/index.ts
@@ -16,6 +16,7 @@ export default class Deploy extends Command {
 
   static flags = {
     file: Flags.string({ char: 'f', description: 'Config file path', required: false }),
+    services: Flags.string({ char: 's', description: 'Service names (separated with commas)', required: false }),
   }
 
   static args = {
@@ -24,13 +25,17 @@ export default class Deploy extends Command {
   async run(): Promise<void> {
     const { flags } = await this.parse(Deploy)
     const config = parseConfig(flags.file || './heroculer.yml')
+    const servicesToDeploy = flags.services?.split(',')
     for (let service of config.services) {
       const serviceName = Object.keys(service)[0];
+      if (servicesToDeploy && !servicesToDeploy.includes(serviceName)) {
+        continue
+      }
       const envVariables = service[serviceName].environment
       const envFilePath = service[serviceName].env_file
       this.appName = service[serviceName].app_name
       this.configEnvVariables(envFilePath, envVariables)
-      ux.action.start(`Start deployment for ${serviceName} microservice`, '', { stdout: true })
+      ux.action.start(`Deployment for ${serviceName} microservice`, '', { stdout: true })
       try {
         const { stdout, stderr } = await exec(`git push ${serviceName}`)
         if (stdout) {
@@ -48,14 +53,14 @@ export default class Deploy extends Command {
   }
 
   async scaleDynos(processes: []) {
-    ux.action.start('scaling dynos...')
+    ux.action.start('Scaling dynos...')
     const scaleCommand = processes.map((x: any) => `${Object.keys(x)[0]}=${x[Object.keys(x)[0]]}`).join(' ')
     await exec(`heroku scale ${scaleCommand} -a ${this.appName}`)
-    ux.action.stop()
+    ux.action.stop('✅ Scaled!')
   }
 
   async configEnvVariables(envFilePath: string, envVariables: EnvVariables): Promise<void> {
-    ux.action.start('set environment variables...')
+    ux.action.start('Set environment variables...')
     const parsed = dotenv.parse(fs.readFileSync(envFilePath, { encoding: 'utf8' }))
     const variablesFromFile: EnvVariables = Object.entries(parsed).map(([k, v]) => ({ [k]: v })) as EnvVariables
     const mergedVariables = [...variablesFromFile, ...envVariables]
@@ -63,6 +68,6 @@ export default class Deploy extends Command {
       const envKey = Object.keys(envVar)[0]
       await exec(`heroku config:set ${envKey}=${envVar[envKey]} -a ${this.appName}`)
     }
-    ux.action.stop()
+    ux.action.stop('✅ Done!')
   }
 }
